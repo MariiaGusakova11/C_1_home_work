49b3b44add2ef909e5c13c5ab2a704a3f07c2fcf
Mariia <mariia.gusakova.msc@gmail.com>
2023-09-21 23:39:46 +0300

    Задача 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (square / number == number)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}
