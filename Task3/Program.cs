c4b7cf44a962a606c241c41c24dc8e9c59dd17dd
Mariia <mariia.gusakova.msc@gmail.com>
2023-09-23 00:58:22 +0300

    Задача

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
 Console.WriteLine("Hello, World!");
+Console.WriteLine("ВВедите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int negative_number = -number;
+    while (negative_number <= number)
+    {
+        Console.WriteLine(negative_number + ", ");
+        negative_number++;
+    }
+
+}
